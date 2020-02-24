039785db49a801f092eb61a84d118d53dc0c7c37
Fox <foxsarh@dunwoody.edu>
2020-02-23 21:34:01 -0600

    created new branch to store changes

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,7 +1,9 @@
 ﻿/*
- * Create a list with all of the player
+ * Create a list with all of the player //clear the list then generate the list
  * Print introduction for user
- * While budget is under 95 million
+ * WHILE (Console.Key != Console.Key X)
+ *    OutputList()
+ * While budget < 95 million && count < 5  && coachPick != x
  *     Get input from user to select a player position from list by entering a # between 1-8
      * If user inputs number x
      *      Display available players with info from x position 
@@ -95,37 +97,15 @@ namespace project1
                                     {"The Best", "2nd Best", "3rd Best", "4th Best", "5th Best"},
                                     {"The Best", "2nd Best", "3rd Best", "4th Best", "5th Best"}
                                   }; */
-            string[,] rosterArray =
-            {
-                {"Joe Burrow", "LSU","26400100","Quaterback","1" },
-                {"Tua Tagovailoa", "Alabama","20300100", "Quaterback","2" },
-                {"Justin Herbet", "Oregon","17420300", "Quaterback", "3" },
-                {"Jordan Love","Utah St", "13100145","Quaterback","4"}
+            //string[,] rosterArray =
+            //{
+            //    {"Joe Burrow", "LSU","Quarterback", "26400100","the best" },
+            //    {"Tua Tagovailoa", "Alabama", "Quarterback", "20300100","the 2nd best" },
+            //    {"Justin Herbet", "Oregon", "Quarterback", "17420300", "the 4th best" },
+            //    {"Jordan Love","Utah St", "Quarterback", "13100145","the 5th best"}
 
-            };
+            //};
 
-            // Create new list to hold only player objects 
-            List<Player> playerList = new List<Player>();
-            List<Player> chosenPlayers = new List<Player>();
-
-            string[] names = { "Joe Burrow", "Tua Tagovailoa" };
-            string[] institutions = { "LSU", "Alabama", "Oregon" };
-            string[] positions = {"Quarterback","Running Back","Wide-Receiver" };
-            double[] salaries = {26400100, 20300100, 17420300 };
-            string[] ranks = { "The Best", "2nd Best", "3rd Best", "4th Best", "5th Best"};
-
-            // Use a loop to assign all player data to player objects 
-            for (var r = 0; r <8; r++) // Outter loop (r stands for Row)
-            {
-                for (var c = 0; c < 8; c++) // Inner loop (c stands for Column)
-                {
-                    Player name = new Player(nameArray[r, c], institutionArray[r, c], positionAray [r, c], salaryArray[r, c],true, rankArray[r,c]);
-                    playerList.Add(name);
-                } // End of Inner Loop
-
-            } // End of Outter Loop
-
-            // Create Welcome Message and Instructions \\
             Console.WriteLine("Welcome to the NFL Draft!");
             Console.WriteLine("You may pick any combination of players");
             Console.WriteLine("Here are the Rules:");
@@ -134,6 +114,128 @@ namespace project1
             Console.WriteLine("\t3. If all 5 of your players are within the top 3 --and--");
             Console.WriteLine("\t\ttheir combined salaries are $65 mil or less,");
             Console.WriteLine("\t\tyou will receive a special alert that your draft is Cost Effective!");
+
+            // Create new list to hold only player objects 
+            List<Player> playerList = new List<Player>()
+            {
+                new Player("Joe Burrow", "LSU","Quarterback", 26400100, true, "the best"),
+                new Player("Tua Tagovailoa", "Alabama", "Quarterback", 20300100, true,"the 2nd best"),
+                new Player("Justin Herbet", "Oregon", "Quarterback", 17420300, true, "the 4th best" )
+            };
+            List<Player> chosenPlayers = new List<Player>();
+
+            //Output the list
+            foreach (Player aPlayer in playerList)
+            {
+                Console.WriteLine(aPlayer.Name, aPlayer.Institution, aPlayer.Salary, aPlayer.Rank);
+            }
+
+            string position = "";
+
+            //User input for position.
+            Console.WriteLine("Please enter position: \n 1.Quarterback\n2.Running Back\n3. Wide Receiver\n");
+            int positionNumber = Convert.ToInt32(Console.ReadLine());
+
+            if( positionNumber == 1)
+            {
+                position = "Quarterback";
+            }
+            else if(positionNumber == 2)
+            {
+                position = "Running Back";
+
+            }else if(positionNumber == 3)
+            {
+                position = "Wide Receiver";
+            }
+            else
+            {
+                Console.WriteLine("Invalid Selection");
+            }
+
+            //Console.WriteLine("Please enter Rank: \n 1.The Best \n2.The Second Best \n3. The third Best\n");
+            //selection 
+            string selection = "the best";
+
+            Player selectedPlayer;
+            //Determine users pick
+            /**foreach (Player aPlayer in playerList)
+            {
+               if((aPlayer.Position == position)  && (aPlayer.Rank == selection))
+                {
+                    chosenPlayers.Add(aPlayer);
+
+                   
+                   
+                }
+               
+            }**/
+
+            for (var i = playerList.Count - 1; i >= 0; i--)
+            {
+                if (playerList[i].Position.Contains(position) && playerList[i].Rank == selection)
+                {
+                    chosenPlayers.Add(playerList[i]);
+                    playerList.RemoveAt(i);
+                }
+            }
+
+
+
+
+
+            Console.Clear();
+            //output all players still remaining
+            Console.WriteLine("These are the players that are availabe to select:");
+            foreach (Player aPlayer in playerList)
+            {
+                Console.WriteLine(aPlayer.Name, aPlayer.Institution, aPlayer.Salary, aPlayer.Rank);
+            }
+
+            //Show players who have been selected
+            Console.WriteLine("These are the players you have selected");
+            foreach (Player aPlayer in chosenPlayers)
+            {
+                Console.WriteLine(aPlayer.Name, aPlayer.Institution, aPlayer.Salary, aPlayer.Rank);
+            }
+
+
+            /*string[] names = { "Joe Burrow", "Tua Tagovailoa" };
+            string[] institutions = { "LSU", "Alabama", "Oregon" };
+            string[] positions = {"Quarterback","Running Back","Wide-Receiver" };
+            double[] salaries = {26400100, 20300100, 17420300 };
+            string[] ranks = { "The Best", "2nd Best", "3rd Best", "4th Best", "5th Best"};
+            */
+
+            // Use a loop to assign all player data to player objects 
+            //for (var r = 0; r <=4; r++) // Outter loop (r stands for Row)
+            //{
+            //    for (var c = 0; c <=4; c++) // Inner loop (c stands for Column)
+            //    {
+            //        Player name = new Player(nameArray[r, c], institutionArray[r, c], positionAray [r, c], salaryArray[r, c],true, rankArray[r,c]);
+            //        Player name = new Player(rosterArray[r, c], rosterArray[r, c], rosterArray[r, c], Convert.ToDouble(rosterArray[r, c]), true, rosterArray[r, c]);
+            //        playerList.Add(name);
+            //    } // End of Inner Loop
+
+            //} // End of Outter Loop
+
+            /*foreach (Player aPlayer in playerList)
+            {
+                Console.WriteLine(aPlayer.Name, aPlayer.Institution, aPlayer.Salary, aPlayer.Rank);
+            }*/
+
+            // Create Welcome Message and Instructions \\
+            //Console.WriteLine("Welcome to the NFL Draft!");
+            //Console.WriteLine("You may pick any combination of players");
+            //Console.WriteLine("Here are the Rules:");
+            //Console.WriteLine("\t1. The total combined salary of all players cannot exceed your $95 million budget");
+            //Console.WriteLine("\t2. You may pick up to 5 players");
+            //Console.WriteLine("\t3. If all 5 of your players are within the top 3 --and--");
+            //Console.WriteLine("\t\ttheir combined salaries are $65 mil or less,");
+            //Console.WriteLine("\t\tyou will receive a special alert that your draft is Cost Effective!");
+
+            //User input for position.
+            Console.WriteLine("1.Quarterback\n2.Running Back\n3. Wide Receiver\n");
             
             // the loop should be the length of the arrays 
             // Loop should be 0-24 (25 long) 
@@ -144,7 +246,7 @@ namespace project1
     {
         public string Name;
         public string Institution;
-        public string Position;
+        public string Position { get; set; }
         public double Salary;
         public bool Availability;
         public string Rank;
@@ -152,9 +254,10 @@ namespace project1
         public Player(string Name, string Inst, string Pos, double Salary, bool Avail, string Rank)
         {
             this.Name = Name;
-            Inst = Institution;
-            Pos = Position;
-            Avail = Availability;
+            this.Institution = Inst;
+            Position = Pos;
+            this.Salary = Salary;
+            Availability = Avail;
             this.Rank = Rank;
         }
 
