598f45985372c0f3bafda2618c2ece39aa1214ac
Fox <foxsarh@dunwoody.edu>
2020-02-28 00:17:44 -0600

    Added a looping structure to wrapper the program so that it repeats as well as added logic to allow player to exit the program with up to 5 players as well less than 5 players

--- project1/Program.cs
+++ project1/Program.cs
@@ -43,28 +43,17 @@ namespace project1
         {
 
             //Declarations
+            List<Player> chosenPlayers;
+            List<Player> playerList;
+            List<Player> costEffectiveList;
             ConsoleKey sentinelValue = ConsoleKey.Enter;
             ConsoleKey userInput;
             // List to hold only chosen players
-            List<Player> chosenPlayers = new List<Player>();
-            // List to hold all player objects
-            List<Player> playerList = new List<Player>()
 
-
-            {
-                new Player("Joe Burrow", "LSU","Quarterback", 26400100, true, "the best"),
-                new Player("Tua Tagovailoa", "Alabama", "Quarterback", 20300100, true,"the 2nd best"),
-                new Player("Justin Herbet", "Oregon", "Quarterback", 17420300, true, "the 4th best" )
-            };
 
             //Instructions
             Console.WriteLine("Welcome to the NFL Draft!");
-            Console.WriteLine("You may pick any combination of players\n");
-            Console.WriteLine("\tHere are the Rules:");
-            Console.WriteLine("\t1. The total combined salary of all players cannot exceed your $95 million budget");
-            Console.WriteLine("\t2. You may pick up to 5 players");
-            Console.WriteLine("\t3. If all 5 of your players are within the top 3 AND their combined salaries are $65 mil or less,");
-            Console.WriteLine("\t   you will receive a special alert that your draft is Cost Effective!\n");
+
 
             Console.WriteLine("To begin the draft press any key");
             Console.WriteLine("To exit the draft press enter");
@@ -72,10 +61,29 @@ namespace project1
             //Get user input to begin draft
             userInput = Console.ReadKey().Key;
             Console.WriteLine(); //Strictly for formatting purposes
-
+            
             //Continue program unless they escape
             while (userInput != sentinelValue)
             {
+                Console.Clear();
+                Console.WriteLine("You may pick any combination of players\n");
+                Console.WriteLine("\tHere are the Rules:");
+                Console.WriteLine("\t1. The total combined salary of all players cannot exceed your $95 million budget");
+                Console.WriteLine("\t2. You may pick up to 5 players");
+                Console.WriteLine("\t3. If all 5 of your players are within the top 3 AND their combined salaries are $65 mil or less,");
+                Console.WriteLine("\t   you will receive a special alert that your draft is Cost Effective!\n");
+               
+                chosenPlayers = new List<Player>();
+                // List to hold all player objects
+                playerList = new List<Player>();
+
+                costEffectiveList = new List<Player>();
+
+                {
+                    new Player("Joe Burrow", "LSU","Quarterback", 26400100, true, "the best"),
+                    new Player("Tua Tagovailoa", "Alabama", "Quarterback", 20300100, true,"the 2nd best"),
+                    new Player("Justin Herbet", "Oregon", "Quarterback", 17420300, true, "the 4th best" )
+                };
 
                 //Output the list of players
                 foreach (Player aPlayer in playerList)
@@ -83,104 +91,141 @@ namespace project1
                     Console.WriteLine(aPlayer.Name + aPlayer.Institution + aPlayer.Position + aPlayer.Salary + aPlayer.Rank);
                 }
 
-                string position = "";
+               
 
                 //User input for position.
-                Console.WriteLine("Please enter the position you wish to draft from: \n1. Quarterback\n2. Running Back\n3. Wide Receiver\n");
-                int positionNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nPlease enter the position you wish to draft from: \n1. Quarterback\n2. Running Back\n3. Wide Receiver\n");
+                int positionNumber = Convert.ToInt32(Console.ReadLine()); //primer
 
-                //Exception Handling
-                while (positionNumber == 1 || positionNumber == 2 || positionNumber == 3 || positionNumber == 4 || positionNumber == 5)
+                while (positionNumber  != -1 && chosenPlayers.Count < 5)
                 {
-                    if (positionNumber == 1)
+
+                    string position = "";
+                    //Exception Handling
+                    while (positionNumber == 1 || positionNumber == 2 || positionNumber == 3 || positionNumber == 4 || positionNumber == 5)
                     {
-                        position = "Quarterback";
-                        break;
-                    }
-                    else if (positionNumber == 2)
-                    {
-                        position = "Running Back";
-                        break;
-
-                    }
-                    else if (positionNumber == 3)
-                    {
-                        position = "Wide Receiver";
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid Selection, please try again & enter a number between 1 and 5");
-                    }
-
-                }//end of while loop
-
-                string userRanking = "";
-
-                Console.WriteLine("Please enter ranking: \n1. The Best\n2. 2nd Best\n3. 3rd Best\n4. 4th Best\n5. 5th best");
-                int rankingNumber = Convert.ToInt32(Console.ReadLine());
-
-                if (rankingNumber == 1)
-                {
-                    userRanking = "the best";
-                }
-                else if (rankingNumber == 2)
-                {
-                    userRanking = "the 2nd best";
-
-                }
-                else if (rankingNumber == 3)
-                {
-                    userRanking = "the 3rd best";
-                }
-
-                else if (rankingNumber == 4)
-                {
-                    userRanking = "the 4th best";
-                }
-
-                else if (rankingNumber == 5)
-                {
-                    userRanking = "the 5th best";
-                }
-
-                else
-                {
-                    Console.WriteLine("Invalid Selection");
-                }
-
-                //While loop go here?? but what for??
-
-                //Logic to determine who user picked
-                for (var i = playerList.Count - 1; i >= 0; i--)
-                    if (playerList[i].Position.Contains(position) && playerList[i].Rank == userRanking)
-                    {
-
-                        if (playerList[i].Availability == true)
+                        if (positionNumber == 1)
                         {
-                            playerList[i].Availability = false;
-                            chosenPlayers.Add(playerList[i]);
-                            playerList.RemoveAt(i);
+                            position = "Quarterback";
+                            break;
                         }
+                        else if (positionNumber == 2)
+                        {
+                            position = "Running Back";
+                            break;
 
+                        }
+                        else if (positionNumber == 3)
+                        {
+                            position = "Wide Receiver";
+                            break;
+                        }
                         else
                         {
-                            Console.WriteLine("You have already chosen this player, please try again");
+                            Console.WriteLine("Invalid Selection, please try again & enter a number between 1 and 5");
                         }
 
+                    }//end of while loop
+
+                    string userRanking = "";
+
+                    Console.WriteLine("Please enter ranking: \n1. The Best\n2. 2nd Best\n3. 3rd Best\n4. 4th Best\n5. 5th best");
+                    int rankingNumber = Convert.ToInt32(Console.ReadLine());
+
+                    if (rankingNumber == 1)
+                    {
+                        userRanking = "the best";
+                    }
+                    else if (rankingNumber == 2)
+                    {
+                        userRanking = "the 2nd best";
+
+                    }
+                    else if (rankingNumber == 3)
+                    {
+                        userRanking = "the 3rd best";
                     }
 
+                    else if (rankingNumber == 4)
+                    {
+                        userRanking = "the 4th best";
+                    }
 
-                //Clear Console of previous data for usability 
-                Console.Clear();
+                    else if (rankingNumber == 5)
+                    {
+                        userRanking = "the 5th best";
+                    }
 
-                //output all players still remaining
-                Console.WriteLine("These are the players that are availabe to select:");
-                playerList.ForEach(i => Console.WriteLine(i.ToString()));
+                    else
+                    {
+                        Console.WriteLine("Invalid Selection");
+                    }
+                    Console.Clear();
+                    //While loop go here?? but what for??
+
+
+
+                    //Logic to determine who user picked
+                    for (var i = playerList.Count - 1; i >= 0; i--)
+                        if (playerList[i].Position.Contains(position) && playerList[i].Rank == userRanking)
+                        {
+
+                            if (playerList[i].Availability == true)
+                            {
+                                playerList[i].Availability = false;
+                                Console.WriteLine(playerList[i].ToString()); 
+                                chosenPlayers.Add(playerList[i]);
+
+                                if (playerList[i].Rank == "the best" || playerList[i].Rank == "the 2nd best" || playerList[i].Rank == "the 3rd best" )
+                                {
+                                    costEffectiveList.Add(playerList[i]);
+                                }
+                                
+
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("You have already chosen this player.");
+                            }
+
+                        }
+                    
+                    Console.WriteLine("");
+                    
+
+                    //output all players still remaining
+                    Console.WriteLine("These are the players that are availabe to select:");
+                    playerList.ForEach(i => {
+                        if (i.Availability == false) {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(i.ToString());
+                            Console.ResetColor();
+
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine(i.ToString());
+                            Console.ResetColor();
+                        }
+
+                        });
+
+                    //User input for position.
+                    Console.WriteLine("\nPlease enter the position you wish to draft from: \n1. Quarterback\n2. Running Back\n3. Wide Receiver\n or enter -1 to end draft");
+                    positionNumber = Convert.ToInt32(Console.ReadLine()); //Primer
+                    //Clear Console of previous data for usability 
+                    Console.Clear();
+                }//End of player selection loop
+
+
+                //Cost Effective
+
 
 
                 //Formatting so it is easier to read 
-                Console.WriteLine();
+                Console.WriteLine("");
 
                 //Show players who have been selected
                 Console.WriteLine("\nThese are the players you have selected");
@@ -190,7 +235,9 @@ namespace project1
                     Console.WriteLine("To finish the draft and exit, press enter");
 
                     userInput = Console.ReadKey().Key;
- 
+                chosenPlayers.Clear();
+                playerList.Clear();
+                costEffectiveList.Clear();
 
             }// End of outer While loop
 
@@ -218,7 +265,11 @@ namespace project1
 
         public override string ToString()
         {
-            return String.Format(Position + " | " + Name + " | " + Institution + " | "  +  Salary.ToString("c") + " | " + Rank);
+        
+                
+                return String.Format(Position + " | " + Name + " | " + Institution + " | " + Salary.ToString("c") + " | " + Rank + "Availability: " + Availability);
+          
+           
         }
 
         public void determineBest (int count)
